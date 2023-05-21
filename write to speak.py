d7c5adf590ab1aaac6c23899b8e47e06adbfff98
parth mistry <75114126+parth-1717@users.noreply.github.com>
2023-05-21 17:35:53 +0530

    Add files via upload

---
+++ write to speak.py
@@ -0,0 +1,14 @@
+# import the pyttsx module inside program
+import pyttsx3
+
+# initializing the module
+engine = pyttsx3.init()
+
+# .say() function is used to speak the text you have written
+# inside the function
+engine.say("Hi parth nice to meet you. what your day. I fell very cool. and you are the best programer in world")
+
+# this is used to process and run the program commands
+engine.runAndWait()
+
+# This was made by Parth Mistry
